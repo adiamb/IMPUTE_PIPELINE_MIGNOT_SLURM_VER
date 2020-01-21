c7a3f98c2504fd2dfb7824b4e8725bafe69151fe
Logan Schneider <loganschneider@users.noreply.github.com>
2020-01-21 10:50:51 -0800

    Update IMPUTE_LOOP_SLURM.sh

--- IMPUTE_LOOP_SLURM.sh
+++ IMPUTE_LOOP_SLURM.sh
@@ -6,7 +6,7 @@
 for i in `seq 0 $2` 
 do
 interval=`echo $i'e6 '$(($i +1))'e6'`
-command=`echo ./impute2 -known_haps_g "$3"_CHR"$1".haps -h /oak/stanford/scg/lab_mignot/IMPUTE_REFERENCE_PHASE1/ALL.chr"$1".integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nomono.haplotypes.gz -l /srv/gsfs0/projects/mignot/IMPUTE_REFERENCE_PHASE1/ALL.chr"$1".integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nomono.legend.gz -m /srv/gsfs0/projects/mignot/IMPUTE_REFERENCE_PHASE1/genetic_map_chr"$1"_combined_b37.txt -int "$interval" -buffer 500 -Ne 20000 -o CHR"$1"_"$3"."$i"`
+command=`echo ./impute2 -known_haps_g "$3"_CHR"$1".haps -h /oak/stanford/scg/lab_mignot/IMPUTE_REFERENCE_PHASE1/ALL.chr"$1".integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nomono.haplotypes.gz -l /oak/stanford/scg/lab_mignot/IMPUTE_REFERENCE_PHASE1/ALL.chr"$1".integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nomono.legend.gz -m /oak/stanford/scg/lab_mignot/IMPUTE_REFERENCE_PHASE1/genetic_map_chr"$1"_combined_b37.txt -int "$interval" -buffer 500 -Ne 20000 -o CHR"$1"_"$3"."$i"`
 touch tmpchr"$1".$i.sh
 chmod 755 tmpchr"$1".$i.sh
 cat > tmpchr"$1".$i.sh <<- EOF
